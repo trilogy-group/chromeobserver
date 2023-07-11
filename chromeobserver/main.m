713fdf5afd966c00929e72d17ba36e41f7448aed
Raul Guerrero <raul.guerrero@devfactory.com>
2023-07-10 21:50:51 -0600

    Initial Commit

---
+++ chromeobserver/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  chromeobserver
+//
+//  Created by Raul Guerrero on 10/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
